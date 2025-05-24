1e3ab39016274f467d3e469df714663c91b867fb
s:103 <2040484356@qq.com>
2025-05-24 18:35:13 +0800

    图表库

--- src/pages/Layout/Home/index.js
+++ src/pages/Layout/Home/index.js
@@ -1,7 +1,35 @@
+import * as echarts from 'echarts';
+import { useEffect } from 'react';
+
 const Home = () => {
+
+  useEffect(() => {
+    const chartDom = document.getElementById('main');
+    const myChart = echarts.init(chartDom);
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <div id="main" style={{width: ""}}>
+      
+      </div>
     </div>
   )
 }
